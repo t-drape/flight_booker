f7a3d00afcf913ffc0dd8a3bcd9f227b5994658b
t-drape <thebatreview1@gmail.com>
2025-03-01 20:47:41 -0700

    Finish step 2

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,15 +3,18 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
 
   def self.get_flight_dates
-    times = Flight.select(:start)
-    times
+    Flight.select(:start)
   end
 
   def flight_date_formatted
-    start.strftime("%m/%d/%y - %H:%M")
+    start.strftime("%m/%d/%y")
   end
 
   def start_stripped
-    start.strftime("%Y%m%d%H%M")
+    start.strftime("%Y%m%d")
+  end
+
+  def id_and_duration
+    "Flight ID: #{id}, Duration: #{duration} Hours"
   end
 end
